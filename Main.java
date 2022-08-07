f7f66b9a363ef594cabcac2c17f30498311bc3fd
dharma610 <dkg06102001@gmail.com>
2022-08-07 23:31:11 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+      // Variables
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    //System.out.print(n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.print("\n");
+    }
+       
+   }
+}
+
